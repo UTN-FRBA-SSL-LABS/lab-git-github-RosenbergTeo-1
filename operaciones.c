8d5cdc971484b8d7aab5edc61a4fd964f10e9a6d
RosenbergTeo <rosenbergteo@gmail.com>
2026-05-04 23:06:06 -0300

    Version conflicto v2

--- operaciones.c
+++ operaciones.c
@@ -21,5 +21,5 @@ int multiplicar(int a, int c) {
 /* ── esPar ────────────────────────────────────────────────────────────────── */
 
 int esPar(int n) {
-    return (n % 2) == 0; /* version main CAMBIO */
+    return (n & 1) == 0; /* version conflicto */
 }
